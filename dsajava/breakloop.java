0b639ead2ceff125a123e8717dc27fd262117de0
abhaymane9404-creator <abhaymane9404@gmail.com>
2026-02-23 09:51:13 +0530

    Added Java loop programs (break, do-while, reverse, loop examples)

---
+++ dsajava/breakloop.java
@@ -0,0 +1,12 @@
+public class breakloop {
+    public static void main(String[] args) {
+        
+        for ( int i=1; i<=10; i++ ) {
+           if (i==3) {
+            break ; 
+           } 
+           System.out.println(i);
+        }
+        System.out.println(" loop is cut ");
+    }
+}
---
+++ dsajava/reversenum.java
@@ -0,0 +1,10 @@
+public class reversenum {    
+public static void main(String[] args) {
+    int n = 1234;
+    while (n > 0) {
+        int last_digit = n%10 ;
+     System.out.print(last_digit);
+        n = n/10 ; 
+    }
+}
+}
